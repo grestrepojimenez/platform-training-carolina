051f81ccf435ea80a3a1862e3eefa95146e561b2
Carolina Uribe Botero <uribebotero.carolin@gmail.com>
2023-11-19 17:11:31 -0500

    creacion del componente MenuBar

---
+++ src/components/Buttons/MenuBar/MenuBar.jsx
@@ -0,0 +1,116 @@
+/* eslint-disable react/prop-types */
+import { useState, useRef, useEffect } from "react";
+import {
+  Button,
+  ClickAwayListener,
+  Grow,
+  MenuItem,
+  MenuList,
+  Paper,
+  Popper,
+} from "@mui/material";
+
+const MenuBar = ({title}) => {
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === "Escape") {
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = useRef(open);
+  useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  return (
+    <div>
+      <Button
+        ref={anchorRef}
+        id="composition-button"
+        aria-controls={open ? "composition-menu" : undefined}
+        aria-expanded={open ? "true" : undefined}
+        aria-haspopup="true"
+        onClick={handleToggle}
+        endIcon={<i className="bx bx-chevron-down" />}
+        color="error"
+        className="capitalize font-light tracking-wider"
+      >
+        {title}
+      </Button>
+      <Popper
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        placement="bottom-start"
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === "bottom-start" ? "left top" : "left bottom",
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList
+                  autoFocusItem={open}
+                  id="composition-menu"
+                  aria-labelledby="composition-button"
+                  onKeyDown={handleListKeyDown}
+                  className="bg-black text-red px-6 capitalize "
+                >
+                  <MenuItem
+                    onClick={handleClose}
+                    className="font-light tracking-wider text-sm"
+                  >
+                    Profile
+                  </MenuItem>
+                  <MenuItem
+                    onClick={handleClose}
+                    className="font-light tracking-wider text-sm"
+                  >
+                    My account
+                  </MenuItem>
+                  <MenuItem
+                    onClick={handleClose}
+                    className="font-light tracking-wider text-sm"
+                  >
+                    Logout
+                  </MenuItem>
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </div>
+  );
+};
+
+export default MenuBar;
--- src/pages/ExercisesPage/ExercisesPage.jsx
+++ src/pages/ExercisesPage/ExercisesPage.jsx
@@ -1,11 +1,29 @@
 import NavBar from "../../layout/NavBar/NavBar";
+import { useInputContext } from "../../hooks/useInputContext";
+import MenuBar from "../../components/Buttons/MenuBar/MenuBar";
 
 const ExercisesPage = () => {
+  const { inputData } = useInputContext(); // Acceder al contexto
+
   return (
     <>
       <header>
-        <NavBar/>
+        <NavBar />
       </header>
+      <main>
+        <div className="mx-auto text-center">
+          <h2 className="capitalize tracking-wider font-medium text-2xl text-center text-red mt-20">
+            caro{inputData.name}
+          </h2>
+          <p>Selecciona los ejercicios del entrenamiento</p>
+        </div>
+
+        <div className="flex justify-center m-10 md:space-x-4 space-x-1">
+          <MenuBar title="selecciona musculo" />
+          <MenuBar title="selecciona equipo" />
+        </div>
+        
+      </main>
     </>
   );
 };
